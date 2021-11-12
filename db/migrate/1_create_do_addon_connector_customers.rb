60db361f48f7e56083acba155fdb245563103564
Scott Miller <scott@helpy.io>
2021-11-12 16:55:45 -0700

    Add additional fields to customer

--- db/migrate/1_create_do_addon_connector_customers.rb
+++ db/migrate/1_create_do_addon_connector_customers.rb
@@ -4,7 +4,10 @@ class CreateDoAddonConnectorCustomers < ActiveRecord::Migration[6.1]
       t.integer :user_id
       t.string :key
       t.json :metadata
-
+      t.string :plan
+      t.string :email
+      t.string :creator_uuid
+      
       t.timestamps
     end
     add_index :do_addon_connector_customers, :key
